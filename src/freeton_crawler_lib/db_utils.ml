5aa840c72e063b6bb93077d5368d179538a8a517
Louis Gesbert <louis.gesbert@ocamlpro.com>
2021-05-25 15:59:29 +0200

    Promote pgocaml preprocessed files
    This renames `src/freeton_crawler_lib/db.ml` to `db.pp.ml`, and similarly for
    `db_utils`, adjusts the `dune` build rules so that the `ppx` is run by hand and
    its results staged (as `db.ml`, `db_utils.ml` resp.)
    
    This removes dependencies towards a running and properly configured pgsql
    instance during compilation as a package, while keeping the current behaviour
    when building "by hand" in developer mode.

--- src/freeton_crawler_lib/db_utils.ml
+++ src/freeton_crawler_lib/db_utils.ml
@@ -1,109 +1,165 @@
-(**************************************************************************)
-(*                                                                        *)
-(*  Copyright (c) 2021 OCamlPro SAS                                       *)
-(*                                                                        *)
-(*  All rights reserved.                                                  *)
-(*  This file is distributed under the terms of the GNU Lesser General    *)
-(*  Public License version 2.1, with the special exception on linking     *)
-(*  described in the LICENSE.md file in the root directory.               *)
-(*                                                                        *)
-(*                                                                        *)
-(**************************************************************************)
-
-let database = ref ( Freeton_crawler_db_versions.Main.database () )
-
+let database = ref (Freeton_crawler_db_versions.Main.database ())
 module PGOCaml = PGOCaml_lwt
 module M = PGOCaml_lwt.M
-
 type database_handle = (string, bool) Hashtbl.t PGOCaml.t
-
-let connect () =
-  PGOCaml.connect ~database:!database ()
-
-let create () =
-  EzPG.createdb !database
-
+let connect () = PGOCaml.connect ~database:(!database) ()
+let create () = EzPG.createdb (!database)
 let (>>=) = M.(>>=)
-(* let return = M.return *)
-
 let connected = ref false
 let dbh_pool : database_handle Lwt_pool.t Lazy.t =
   let validate conn =
-    PGOCaml.alive conn >>= fun is_alive ->
-    PGOCaml.debug "[Reader] Validate connection : [%b]\n%!" is_alive ;
-    M.return is_alive in
+    (PGOCaml.alive conn) >>=
+      (fun is_alive ->
+         PGOCaml.debug "[Reader] Validate connection : [%b]\n%!" is_alive;
+         M.return is_alive) in
   let check _conn is_ok =
-    PGOCaml.debug "[Reader] Check connection.\n%!" ;
-    is_ok false in
+    PGOCaml.debug "[Reader] Check connection.\n%!"; is_ok false in
   let dispose conn =
-    PGOCaml.debug "[Reader] Dispose connection.\n%!" ;
-    PGOCaml.close conn in
-  lazy begin
-    M.pool_create ~check ~validate ~dispose 20 (fun () ->
-        connected := true;
-        connect ()
-      )
-  end
-
+    PGOCaml.debug "[Reader] Dispose connection.\n%!"; PGOCaml.close conn in
+  lazy
+    (M.pool_create ~check ~validate ~dispose 20
+       (fun () -> connected := true; connect ()))
 let (let>>>) _ f =
-  M.pool_use ( Lazy.force dbh_pool )
-    (fun dbh -> Lwt.bind (Lwt.return dbh) f)
-
+  M.pool_use (Lazy.force dbh_pool) (fun dbh -> Lwt.bind (Lwt.return dbh) f)
 open Lwt_utils
-
-let try_transaction(* _no_res *) f =
-  let>>> dbh = () in
-  let> () = [%pgsql dbh "BEGIN"] in
-  Lwt.catch (fun () ->
-      let> r = f dbh in
-      let> () = [%pgsql dbh "COMMIT"] in
-      Lwt.return (Ok r)
-    )
+let try_transaction f =
+  let>>> dbh = ()
+   in
+  let> () =
+    PGOCaml.bind
+      (let dbh = dbh in
+       let params : string option list list = [] in
+       let split = [`Text "BEGIN"] in
+       let i = ref 0 in
+       let j = ref 0 in
+       let query =
+         String.concat ""
+           (List.map
+              (function
+               | `Text text -> text
+               | `Var (_varname, false, _) ->
+                   let () = incr i in
+                   let () = incr j in "$" ^ (string_of_int j.contents)
+               | `Var (_varname, true, _) ->
+                   let param = List.nth params i.contents in
+                   let () = incr i in
+                   "(" ^
+                     ((String.concat ","
+                         (List.map
+                            (fun _ ->
+                               let () = incr j in
+                               "$" ^ (string_of_int j.contents)) param))
+                        ^ ")")) split) in
+       let params = List.flatten params in
+       let name = "ppx_pgsql." ^ (Digest.to_hex (Digest.string query)) in
+       let hash =
+         try PGOCaml.private_data dbh
+         with
+         | Not_found ->
+             let hash = Hashtbl.create 17 in
+             (PGOCaml.set_private_data dbh hash; hash) in
+       let is_prepared = Hashtbl.mem hash name in
+       PGOCaml.bind
+         (if not is_prepared
+          then
+            PGOCaml.bind (PGOCaml.prepare dbh ~name ~query ())
+              (fun () -> Hashtbl.add hash name true; PGOCaml.return ())
+          else PGOCaml.return ())
+         (fun () -> PGOCaml.execute_rev dbh ~name ~params ()))
+      (fun _rows -> PGOCaml.return ())
+   in
+  Lwt.catch
+    (fun () ->
+       let> r = f dbh
+        in
+       let> () =
+         PGOCaml.bind
+           (let dbh = dbh in
+            let params : string option list list = [] in
+            let split = [`Text "COMMIT"] in
+            let i = ref 0 in
+            let j = ref 0 in
+            let query =
+              String.concat ""
+                (List.map
+                   (function
+                    | `Text text -> text
+                    | `Var (_varname, false, _) ->
+                        let () = incr i in
+                        let () = incr j in "$" ^ (string_of_int j.contents)
+                    | `Var (_varname, true, _) ->
+                        let param = List.nth params i.contents in
+                        let () = incr i in
+                        "(" ^
+                          ((String.concat ","
+                              (List.map
+                                 (fun _ ->
+                                    let () = incr j in
+                                    "$" ^ (string_of_int j.contents)) param))
+                             ^ ")")) split) in
+            let params = List.flatten params in
+            let name = "ppx_pgsql." ^ (Digest.to_hex (Digest.string query)) in
+            let hash =
+              try PGOCaml.private_data dbh
+              with
+              | Not_found ->
+                  let hash = Hashtbl.create 17 in
+                  (PGOCaml.set_private_data dbh hash; hash) in
+            let is_prepared = Hashtbl.mem hash name in
+            PGOCaml.bind
+              (if not is_prepared
+               then
+                 PGOCaml.bind (PGOCaml.prepare dbh ~name ~query ())
+                   (fun () -> Hashtbl.add hash name true; PGOCaml.return ())
+               else PGOCaml.return ())
+              (fun () -> PGOCaml.execute_rev dbh ~name ~params ()))
+           (fun _rows -> PGOCaml.return ())
+        in Lwt.return (Ok r))
     (fun exn ->
-       let> () = [%pgsql dbh "ROLLBACK"] in
-       Lwt.return (Error exn)
-    )
-
+       let> () =
+         PGOCaml.bind
+           (let dbh = dbh in
+            let params : string option list list = [] in
+            let split = [`Text "ROLLBACK"] in
+            let i = ref 0 in
+            let j = ref 0 in
+            let query =
+              String.concat ""
+                (List.map
+                   (function
+                    | `Text text -> text
+                    | `Var (_varname, false, _) ->
+                        let () = incr i in
+                        let () = incr j in "$" ^ (string_of_int j.contents)
+                    | `Var (_varname, true, _) ->
+                        let param = List.nth params i.contents in
+                        let () = incr i in
+                        "(" ^
+                          ((String.concat ","
+                              (List.map
+                                 (fun _ ->
+                                    let () = incr j in
+                                    "$" ^ (string_of_int j.contents)) param))
+                             ^ ")")) split) in
+            let params = List.flatten params in
+            let name = "ppx_pgsql." ^ (Digest.to_hex (Digest.string query)) in
+            let hash =
+              try PGOCaml.private_data dbh
+              with
+              | Not_found ->
+                  let hash = Hashtbl.create 17 in
+                  (PGOCaml.set_private_data dbh hash; hash) in
+            let is_prepared = Hashtbl.mem hash name in
+            PGOCaml.bind
+              (if not is_prepared
+               then
+                 PGOCaml.bind (PGOCaml.prepare dbh ~name ~query ())
+                   (fun () -> Hashtbl.add hash name true; PGOCaml.return ())
+               else PGOCaml.return ())
+              (fun () -> PGOCaml.execute_rev dbh ~name ~params ()))
+           (fun _rows -> PGOCaml.return ())
+        in Lwt.return (Error exn))
 let transaction f =
-  let> res = try_transaction f in
-  match res with
-  | Ok r -> Lwt.return r
-  | Error exn -> raise exn
-
-(* let transaction f =
- *   let>>> dbh = () in
- *   let> () = [%pgsql dbh "BEGIN"] in
- *   Lwt.catch (fun () ->
- *       let> r = f dbh in
- *       let> () = match r with
- *         | Ok _ -> [%pgsql dbh "COMMIT"]
- *         | Error _ -> [%pgsql dbh "ROLLBACK"] in
- *       Lwt.return r
- *     )
- *     (fun exn ->
- *        let> () = [%pgsql dbh "ROLLBACK"] in
- *        Format.eprintf "Error in DB transaction: @[%s@]@."
- *          (Printexc.to_string exn);
- *        Error.server_error exn "Error in DB transaction, was rolled back"
- *     ) *)
-
-(*
-type pg_error =
-  | UniqueViolation of string
-  | OtherPGError of string
-  | OtherError of exn
-
-let pg_error_of_exn = function
-  | PGOCaml.PostgreSQL_Error (_, fields) ->
-    let msg = match List.assoc_opt 'M' fields with
-      | None -> "Unknown Postgres Error"
-      | Some msg -> msg in
-    (match List.assoc_opt 'C' fields with
-     | Some "23505" (* unique_violation *) ->
-       UniqueViolation msg
-     | _ -> OtherPGError msg
-    )
-  | e -> OtherError e
-*)
-
-let ( let> ) = ( let> )
+  let> res = try_transaction f
+   in match res with | Ok r -> Lwt.return r | Error exn -> raise exn
+let (let>) = (let>)
