f196ce0f1f710b6d53a9f2a4a3a636b978efffd3
Fabrice Le Fessant <fabrice.le_fessant@origin-labs.com>
2021-05-27 17:34:37 +0200

    Remove ft-crawler and make pgocaml mandatory

--- src/freeton_wallet_lib/commandCrawler.ml
+++ src/freeton_wallet_lib/commandCrawler.ml
@@ -13,15 +13,97 @@
 open Ezcmd.V2
 open EZCMD.TYPES
 
+let (let>) = Db_utils.(let>)
+
+let action_start ~account =
+  ProcessManager.start
+    ( Printf.sprintf "crawler_%s" account )
+    [| Sys.argv.(0) ; "crawler" ; account |]
+let action_status ~account =
+  ProcessManager.status
+    ( Printf.sprintf "crawler_%s" account )
+let action_stop ~account =
+  ProcessManager.stop
+    ( Printf.sprintf "crawler_%s" account )
+
+
+let action_crawler ~url ~account ~address ~contract =
+
+  let abifile = Misc.get_contract_abifile contract in
+  let abi = EzFile.read_file abifile in
+
+  Lwt_main.run (
+    let> () = ProcessManager.set_worker
+        ( Printf.sprintf "crawler_%s" account )
+    in
+    Freeton_crawler.main ~url ~address ~abi)
+
+let check_database database dropdb =
+  Db_utils.database := database ;
+  if dropdb then EzPG.dropdb database;
+  let dbh =
+    match PGOCaml.connect ~database () with
+    | exception _exn ->
+        Db_utils.create ();
+        PGOCaml.connect ~database ()
+    | dbh -> dbh
+  in
+  EzPG.upgrade_database
+    ~upgrades:Freeton_crawler_db_versions.Main.upgrades
+    dbh;
+  PGOCaml.close dbh
+
+let get ~account =
+  match account with
+  | None -> Error.raise "You must specify an account to crawl"
+  | Some account ->
+
+      let config = Config.config () in
+      let net = Config.current_network config in
+      let node = Config.current_node config in
+      let key = Misc.find_key_exn net account in
+      let acc = Misc.get_key_account_exn key in
+      match acc with
+        { acc_address = address ;
+          acc_contract = Some contract ; _ }
+        ->
+          account, address, contract, node.node_url
+      | _ ->
+          Error.raise "Contract for address is not known"
+
+
+type action =
+  | Crawler
+  | Start
+  | Status
+  | Stop
+
+let run_action ~dropdb ~account ~action =
+  let ( account, address, contract, url ) = get ~account in
+  match action with
+  | Crawler ->
+      check_database account dropdb ;
+      action_crawler ~account ~address ~contract ~url
+  | Start ->
+      check_database account dropdb ;
+      Lwt_main.run ( action_start ~account )
+  | Status ->
+      Db_utils.database := account ;
+      Lwt_main.run ( action_status ~account )
+  | Stop ->
+      Db_utils.database := account ;
+      Lwt_main.run ( action_stop ~account )
+
+
 let cmd =
 
   let dropdb = ref false in
   let account = ref None in
-  let action = ref Crawler.Crawler in
+  let action = ref Crawler in
   EZCMD.sub
     "crawler"
     (fun () ->
-       Crawler.action ~dropdb:!dropdb ~account:!account ~action:!action
+       run_action ~dropdb:!dropdb ~account:!account ~action:!action
     )
     ~doc: "Crawl all transactions to an address and fill a psql database"
     ~args:[
