70cbafc370e2c41912e4f38f3568e0899ba2870e
Fabrice Le Fessant <fabrice.le_fessant@origin-labs.com>
2021-07-19 17:47:49 +0200

    Depends on ocaml_solidity.0.3.0

--- scripts/contestVote.sh
+++ scripts/contestVote.sh
@@ -14,13 +14,16 @@ fi
 
 COMMENT=$(xxd -p "$COMMENTS_FILE" | tr -d '\n')
 
-echo ft account --create contest --address "$CONTEST_ADDR" -f --contract FreeTonContest
-ft account --create contest --address "$CONTEST_ADDR" -f --contract FreeTonContest
+echo ft switch to mainnet
+ft switch to mainnet 
 
-echo ft account contest
-ft account contest
+echo ft account create contest --address "$CONTEST_ADDR" -f --contract FreeTonContest
+ft account create contest --address "$CONTEST_ADDR" -f --contract FreeTonContest || exit 2
+
+echo ft account info contest
+ft account info contest || exit 2
 
 echo ft call --sign $JURY_ACCOUNT contest voteForCommented \
       "{\"id\":$SUBMISSION_ID,\"mark\":$MARK,\"comment\":\"$COMMENT\"}"
 ft call --sign $JURY_ACCOUNT contest voteForCommented \
-	    "{\"id\":$SUBMISSION_ID,\"mark\":$MARK,\"comment\":\"$COMMENT\"}"
+	    "{\"id\":$SUBMISSION_ID,\"mark\":$MARK,\"comment\":\"$COMMENT\"}" || exit 2
