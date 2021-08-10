131f218282c7057e4024b5f62ea0b40537aaee7a
Fabrice Le Fessant <fabrice.le_fessant@origin-labs.com>
2021-08-10 22:10:25 +0200

    fix docker scripts

--- docker/ft.bat
+++ docker/ft.bat
@@ -1,2 +1,7 @@
 @ECHO OFF
-docker run %FT_DOCKER% -v /var/run/docker.sock:/var/run/docker.sock -v %HOME%/.ft:/user/.ft -v %cd%:/local --network host ocamlpro/ft:latest /bin/ft %*
+
+setlocal enableextensions
+md %HOME%/.ft
+endlocal
+
+docker run %FT_DOCKER% -v /var/run/docker.sock:/var/run/docker.sock -v %HOME%:/user -v %cd%:/local --network host ocamlpro/ft:latest /bin/ft %*
