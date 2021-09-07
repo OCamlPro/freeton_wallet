78d06e80b8a883a48c1745d09fb422f97f38b964
Fabrice Le Fessant <fabrice.le_fessant@origin-labs.com>
2021-09-07 11:49:38 +0200

    add 'ft account state' and change 'ft account info'

--- src/freeton_wallet_lib/commandAccountInfo.ml
+++ src/freeton_wallet_lib/commandAccountInfo.ml
@@ -12,171 +12,77 @@
 
 open Ezcmd.V2
 open EZCMD.TYPES
+
+open Ton_sdk.TYPES
 open Types
 
-(*
-Known code hashes:
-* DePool:
-14e20e304f53e6da152eb95fffc993dbd28245a775d847eed043f7c78a503885
-* SetcodeMultisigWallet2:
-207dc560c5956de1a2c1479356f8f3ee70a59767db2bf4788b1d61ad42cdad82
-* Giver:
-fdfab26e1359ddd0c247b0fb334c2cc3943256a263e75d33e5473567cbe2c124
-
-*)
-
-let get_account_info config address =
-  let addr = Misc.raw_address address in
-  let open Ton_sdk in
-  let level = if !Globals.verbosity > 1 then 3 else 1 in
-  match
-    Utils.post config
-      ( REQUEST.account ~level addr )
-  with
-  | [] -> None
-  |  [ account ] ->
-      if !Globals.verbosity > 1 then
-        Format.printf "%s@."
-          (EzEncoding.construct ~compact:false ENCODING.accounts_enc [account]
-          );
-      begin
-        match account.acc_code_hash with
-        | None -> ()
-        | Some code_hash ->
-            match address with
-            | RawAddress _ -> ()
-            | Account acc ->
-                match acc.acc_contract with
-                | Some _ -> ()
-                | None ->
-                    match Misc.contract_of_code_hash ~code_hash with
-                    | None -> ()
-                    | Some contract ->
-                        Printf.eprintf "Setting contract %S for %s\n%!"
-                          contract acc.acc_address;
-                        acc.acc_contract <- Some contract;
-                        config.modified <- true
-      end;
-      Some account
-  | _ -> assert false
-
-
-let get_account_info config ~name ~address =
-  match get_account_info config address with
-  | None ->
-      Printf.printf "Account %S: not yet created (empty balance)\n%!" name
-  | Some account ->
-      Printf.printf "Account %S: %s\n%!" name
-        (match account.acc_balance with
-         | None -> "no balance"
-         | Some n ->
-             Printf.sprintf "%s TONs (%s)"
-               (Misc.string_of_nanoton (Z.to_int64 n))
-               (match account.acc_type_name with
-                | None -> "Non Exists"
-                | Some s ->
-                    match address with
-                    | Account { acc_contract = Some contract; _ } ->
-                        Printf.sprintf "%s: %s" contract s
-                    | _ -> s
-               ))
-
-let get_key_info config key ~info =
-  if info then
+let get_key_info key ~json ~secrets =
+  if json then
     let json = EzEncoding.construct ~compact:false Encoding.key key in
     Printf.printf "%s\n%!" json
-  else
-    let address = match key.key_account with
-      | None ->
-          Error.raise "Address %s has no address (use genaddr before)"
-            key.key_name
-      | Some account -> Account account
-    in
-    get_account_info config ~address ~name:key.key_name
+  else begin
+    Printf.printf "Name: %s\n" key.key_name ;
+    Option.iter (fun acc ->
+        Printf.printf "  %s\n%!" acc.acc_address;
+        Option.iter (fun s ->
+            Printf.printf "  Contract: %s\n" s
+          ) acc.acc_contract
+      ) key.key_account;
+    Option.iter (fun pair ->
+        Printf.printf "  Public: 0x%s\n" pair.public ;
+        if secrets then
+          Option.iter (fun s ->
+              Printf.printf "  Secret: %s\n" s ;
+            ) pair.secret
+      ) key.key_pair ;
+    if secrets then
+      Option.iter (fun s ->
+          Printf.printf "  Passphrase:\n   %s\n" s) key.key_passphrase;
+    Printf.printf "%!";
+  end
 
-let get_account_info accounts ~info =
+let get_account_info accounts ~json ~secrets =
 
   let config = Config.config () in
   let net = Config.current_network config in
-  if info then
-    match accounts with
-    | [] -> List.iter (fun key ->
-        match key.key_account with
-        | None -> ()
-        | Some _ ->
-            get_key_info config key ~info
-      ) net.net_keys
-    | names ->
-        List.iter (fun name ->
-            match Misc.find_key net name with
-            | None ->
-                Error.raise "No key %S in network %S" name net.net_name
-            | Some key ->
-                get_key_info config key ~info
-          ) names
-  else
-    match accounts with
-    | [] ->
-        List.iter (fun key ->
-            match key.key_account with
-            | None -> ()
-            | Some account ->
-                get_account_info config
-                  ~address:( Account account ) ~name:key.key_name
-          ) net.net_keys
-    | _ ->
-        List.iter (fun account ->
-            let address = Utils.address_of_account net account in
-            get_account_info config ~address ~name:account
-          ) accounts
+  match accounts with
+  | [] -> List.iter (fun key ->
+      match key.key_account with
+      | None -> ()
+      | Some _ ->
+          get_key_info key ~json ~secrets
+    ) net.net_keys
+  | names ->
+      List.iter (fun name ->
+          match Misc.find_key net name with
+          | None ->
+              Error.raise "No key %S in network %S" name net.net_name
+          | Some key ->
+              get_key_info key ~json ~secrets
+        ) names
 
-let get_live accounts =
-  let config = Config.config () in
-  let net = Config.current_network config in
-  let host = match net.net_name with
-    | "mainnet" -> "ton.live"
-    | "testnet" -> "net.ton.live"
-    | _ -> assert false
-  in
-  List.iter (fun account ->
-      let address = Utils.address_of_account net account in
-      let addr = Misc.raw_address address in
-      let url = Printf.sprintf
-          "https://%s/accounts/accountDetails?id=%s" host addr in
-      Misc.call [ "xdg-open" ; url ]
-    ) accounts
-
-
-let action accounts ~info ~live =
-  if live then
-    get_live accounts
-  else
-    get_account_info accounts ~info
+let action accounts ~json ~secrets =
+  get_account_info accounts ~json ~secrets
 
 let cmd =
   let accounts = ref [] in
-  let info = ref false in
-  let live = ref false in
+  let secrets = ref false in
+  let json = ref false in
   EZCMD.sub
     "account info"
     (fun () -> action
-        !accounts
-        ~info:!info
-        ~live:!live
+        !accounts ~json:!json ~secrets:!secrets
     )
     ~args:
       [ [],
         Arg.Anons (fun args -> accounts := args),
         EZCMD.info "Name of account" ;
 
-        [ "all" ] ,
-        Arg.Set info,
-        EZCMD.info "Display all account parameters" ;
+        [ "json" ], Arg.Set json,
+        EZCMD.info "Print in json format";
 
-        [ "live" ],
-        Arg.Set live,
-        EZCMD.info "Open block explorer on address";
-
+        [ "S"; "secrets" ], Arg.Set secrets,
+        EZCMD.info "Print passphrase and secret key (default with --json)";
       ]
     ~man:[
       `S "DESCRIPTION";
@@ -184,7 +90,6 @@ let cmd =
         `P "This command displays information on given accounts, either locally or from the blockchain";
         `P "Examples:";
         `Pre {|ft account info MY-ACCOUNT|};
-        `Pre {|ft account info MY-ACCOUNT --all|}
       ];
 
     ]
---
+++ src/freeton_wallet_lib/commandAccountState.ml
@@ -0,0 +1,143 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  Copyright (c) 2021 OCamlPro SAS                                       *)
+(*                                                                        *)
+(*  All rights reserved.                                                  *)
+(*  This file is distributed under the terms of the GNU Lesser General    *)
+(*  Public License version 2.1, with the special exception on linking     *)
+(*  described in the LICENSE.md file in the root directory.               *)
+(*                                                                        *)
+(*                                                                        *)
+(**************************************************************************)
+
+open Ezcmd.V2
+open EZCMD.TYPES
+open Types
+
+let get_account_info config address =
+  let addr = Misc.raw_address address in
+  let open Ton_sdk in
+  let level = if !Globals.verbosity > 1 then 3 else 1 in
+  match
+    Utils.post config
+      ( REQUEST.account ~level addr )
+  with
+  | [] -> None
+  |  [ account ] ->
+      if !Globals.verbosity > 1 then
+        Format.printf "%s@."
+          (EzEncoding.construct ~compact:false ENCODING.accounts_enc [account]
+          );
+      begin
+        match account.acc_code_hash with
+        | None -> ()
+        | Some code_hash ->
+            match address with
+            | RawAddress _ -> ()
+            | Account acc ->
+                match acc.acc_contract with
+                | Some _ -> ()
+                | None ->
+                    match Misc.contract_of_code_hash ~code_hash with
+                    | None -> ()
+                    | Some contract ->
+                        Printf.eprintf "Setting contract %S for %s\n%!"
+                          contract acc.acc_address;
+                        acc.acc_contract <- Some contract;
+                        config.modified <- true
+      end;
+      Some account
+  | _ -> assert false
+
+
+let get_account_info config ~name ~address =
+  match get_account_info config address with
+  | None ->
+      Printf.printf "Account %S: not yet created (empty balance)\n%!" name
+  | Some account ->
+      Printf.printf "Account %S: %s\n%!" name
+        (match account.acc_balance with
+         | None -> "no balance"
+         | Some n ->
+             Printf.sprintf "%s TONs (%s)"
+               (Misc.string_of_nanoton (Z.to_int64 n))
+               (match account.acc_type_name with
+                | None -> "Non Exists"
+                | Some s ->
+                    match address with
+                    | Account { acc_contract = Some contract; _ } ->
+                        Printf.sprintf "%s: %s" contract s
+                    | _ -> s
+               ))
+
+let get_account_info accounts =
+
+  let config = Config.config () in
+  let net = Config.current_network config in
+  match accounts with
+  | [] ->
+      List.iter (fun key ->
+          match key.key_account with
+          | None -> ()
+          | Some account ->
+              get_account_info config
+                ~address:( Account account ) ~name:key.key_name
+        ) net.net_keys
+  | _ ->
+      List.iter (fun account ->
+          let address = Utils.address_of_account net account in
+          get_account_info config ~address ~name:account
+        ) accounts
+
+let get_live accounts =
+  let config = Config.config () in
+  let net = Config.current_network config in
+  let host = match net.net_name with
+    | "mainnet" -> "ton.live"
+    | "testnet" -> "net.ton.live"
+    | _ -> assert false
+  in
+  List.iter (fun account ->
+      let address = Utils.address_of_account net account in
+      let addr = Misc.raw_address address in
+      let url = Printf.sprintf
+          "https://%s/accounts/accountDetails?id=%s" host addr in
+      Misc.call [ "xdg-open" ; url ]
+    ) accounts
+
+
+let action accounts ~live =
+  if live then
+    get_live accounts
+  else
+    get_account_info accounts
+
+let cmd =
+  let accounts = ref [] in
+  let live = ref false in
+  EZCMD.sub
+    "account state"
+    (fun () -> action !accounts ~live:!live
+    )
+    ~args:
+      [ [],
+        Arg.Anons (fun args -> accounts := args),
+        EZCMD.info "Name of account" ;
+
+        [ "live" ],
+        Arg.Set live,
+        EZCMD.info "Open block explorer on address";
+
+      ]
+    ~man:[
+      `S "DESCRIPTION";
+      `Blocks [
+        `P "This command displays the current states of the given accounts from the blockchain";
+        `P "Examples:";
+        `Pre {|ft account state MY-ACCOUNT|};
+        `Pre {|ft account state MY-ACCOUNT -v|}
+      ];
+
+    ]
+    ~doc:
+      "Get account info (local or from blockchain)."
